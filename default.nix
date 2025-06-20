9908be3e6c309b13c7adf63abdcb2f08069d9aa5
BLKnowledge <111303676+BLKnowledge@users.noreply.github.com>
2025-06-19 20:28:46 -0500

    updated
    update default.nix for building with nixpkgs.

--- default.nix
+++ default.nix
@@ -1,30 +1,9 @@
-{ lib, fetchFromGitHub, rustPlatform, pkgs }:
+{ pkgs ? import <nixpkgs> { }, src ? ./. }:
 
-rustPlatform.buildRustPackage rec {
-    pname = "netscanner";
-    version = "0.6.3";
-    buildInputs = with pkgs; [
-
-    ];
-
-    nativeBuildInputs = with pkgs; [
-
-    ];
-
-    src = fetchFromGitHub {
-     owner = "BLKnowledge";
-     repo = pname;
-     rev = version;
-     hash = "sha256-DVucFlvUciKyfTae1LSd9yUC+3Iff4lEE1jWBsOxz/s=";
-    };
-
-    cargoHash = "sha256-3d8b0c408f0272336c03e6039e0c4611b7c75ad3d298d2cba2fc33336add7def=";
-
-
-    meta = with lib; {
-        description = "Terminal Network scanner & diagnostic tool with modern TUI (terminal user interface). GitHub: https://github.com/Chleba/netscanner";
-        homepage = "https://lib.rs/crates/netscanner";
-        license = licenses.unlicense;
-        maintainers = [];
-    };
+let theSource = src; in
+pkgs.rustPlatform.buildRustPackage rec {
+  pname = "netscanner";
+  version = "0.6.3";
+  src = pkgs.lib.cleanSource "${theSource}";
+  cargoLock.lockFile = "${theSource}/Cargo.lock";
 }
